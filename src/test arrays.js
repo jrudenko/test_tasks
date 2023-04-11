bcbaab46fad44ce334e07ae96122089d89314cbd
Julia Rudenko <yrudenko@i.ua>
2023-04-11 10:59:40 +0300

    start

---
+++ src/test arrays.js
@@ -0,0 +1,13 @@
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  if (password === ADMIN_PASSWORD) {
+    return 'Welcome!';
+  } else error.message;
+
+  return 'Access denied, wrong password!';
+}
+// checkPassword('mangohackzor');
+
+// checkPassword('polyhax');
+console.log(checkPassword('jqueryismyjam'));
+console.log(ADMIN_PASSWORD);
